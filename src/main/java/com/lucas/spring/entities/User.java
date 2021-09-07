3b5c68821e63901a80d38005aca4bec8709c9a3a
Lucns7 <lucns7@hotmail.com>
2021-09-07 13:51:18 -0300

    H2 database, test profile, JPA

--- src/main/java/com/lucas/spring/entities/User.java
+++ src/main/java/com/lucas/spring/entities/User.java
@@ -3,11 +3,18 @@ package com.lucas.spring.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private Integer id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -16,7 +23,8 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(Integer id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -24,11 +32,11 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -80,5 +88,4 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
